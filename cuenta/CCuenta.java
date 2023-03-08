2988f6a3efa0873b0f3841bbf7dc61f849336a98
FranS <franjs.dam@gmail.com>
2023-03-08 20:54:42 +0100

    Se genera la documentacion en javaDoc de la clase CCUenta

--- cuenta/CCuenta.java
+++ cuenta/CCuenta.java
@@ -1,16 +1,44 @@
 package cuenta;
+
+/**
+ * Esta clase represente una cuenta corriente.
+ * @author fran_
+ *
+ */
 public class CCuenta {
 
 
+	/**
+	 * Representa el nombre de la persona titular de la cuenta
+	 */
     private String nombre;
+    /**
+     * Representa de manera inequivoca la cuenta.
+     */
     private String cuenta;
+    /**
+     * Representa el saldo disponible
+     */
     private double saldo;
-    private double tipoInterés;
+    /**
+     * Representa el tipo de interés asociado.
+     */
+    private double tipoInteres;
 
+    /**
+     * Constructor vacío.
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Constructor con parámetros
+     * @param nom nombre
+     * @param cue cuenta
+     * @param sal saldo
+     * @param tipo tipoInteres
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -20,43 +48,84 @@ public class CCuenta {
     
    // getters y setters 
 
+    /**
+     * Metodo que devuelve el atributo nombre.
+     * @return nombre titular de la cuenta
+     */
     public String getNombre() {
 		return nombre;
 	}
 
+    /**
+     * Metodo para modificar el atributo nombre.
+     * @param nombre nuevo
+     */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Devuelve el atributo cuenta.
+	 * @return cuenta
+	 */
 	public String getCuenta() {
 		return cuenta;
 	}
 
+	/**
+	 * Metodo para modificar el atributo cuenta
+	 * @param cuenta nueva
+	 */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
+	/**
+	 * Devuelve el valor de saldo
+	 * @return saldo disponible
+	 */
 	public double getSaldo() {
 		return saldo;
 	}
 
+	/**
+	 * Metodo para modificar el saldo disponible
+	 * @param saldo nuevo
+	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
-	public double getTipoInterés() {
-		return tipoInterés;
+	/**
+	 * Devuelve el tipo de interes asociado a la cuenta
+	 * @return tipoInteres
+	 */
+	public double getTipoInteres() {
+		return tipoInteres;
 	}
 
-	public void setTipoInterés(double tipoInterés) {
-		this.tipoInterés = tipoInterés;
+	/**
+	 * Modifica el tipo de interés.
+	 * @param tipoInteres nuevo
+	 */
+	public void setTipoInteres(double tipoInteres) {
+		this.tipoInteres = tipoInteres;
 	}
 
+	/**
+	 * Deveulve el saldo. Igual que getSaldo()
+	 * @return saldo disponible
+	 */
 	public double estado()
     {
         return saldo;
     }
 
+	/**
+	 * Metodo que simula el ingreso de una cantidad a la cuenta.   
+	 * @param cantidad a ingresar
+	 * @throws Exception Se lanza en caso de intentar ingresar una cantidad negativa.
+	 */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -64,6 +133,11 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     * Metodo que simula el reintegro de una cantidad de la cuenta.
+     * @param cantidad a retirar
+     * @throws Exception Se lanza en caso de intentar retirar una cantidad negativa o superior al saldo disponible.
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
